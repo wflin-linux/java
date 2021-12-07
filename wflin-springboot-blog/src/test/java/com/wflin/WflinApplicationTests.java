889550649ab18e3bbb943fc036c58376b00beace
wflin-linux <2424357107@qq.com>
2021-12-07 13:26:27 +0800

    blogSpringboot-boke

---
+++ wflin-springboot-blog/src/test/java/com/wflin/WflinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wflin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WflinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
