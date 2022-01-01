33d7b7667116aa3839a27166c0ff0b087e21579e
wflin-linux <2424357107@qq.com>
2022-01-01 21:33:10 +0800

    fix email

--- wflin-springboot-blog/src/main/java/com/wflin/Config/MailTool.java
+++ wflin-springboot-blog/src/main/java/com/wflin/Config/MailTool.java
@@ -18,9 +18,9 @@ public class MailTool {
             // 收件人的邮箱
             email.addTo(emailaddress);
             // 发送人的邮箱
-            email.setFrom("2424357107@qq.com");
+            email.setFrom("你的邮箱");
             // 如果需要认证信息的话，设置认证：用户名-密码。分别为发件人在邮件服务器上的注册名称和密码
-            email.setAuthentication("2424357107@qq.com", "vtpasugasgxwdijb");
+            email.setAuthentication("发件邮箱", "发件邮箱授权码");
             // 要发送的邮件主题
             email.setSubject("这是一个登录验证码");
             // 要发送的信息，可以使用了HtmlEmail，实现邮件内容中使用HTML标签
